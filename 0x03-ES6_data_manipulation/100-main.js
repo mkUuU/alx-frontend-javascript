7e2a00bc64e1454436239e4d02e21d6a66d08d95
root <gathecanjino@gmail.com>
2024-07-02 20:14:01 +0000

    data manipulations

---
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -0,0 +1,18 @@
+import { queryAPI, weakMap } from "./100-weak.js";
+
+const endpoint = { protocol: 'http', name: 'getUsers' };
+
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint); // This will throw an error
+
+console.log(weakMap.get(endpoint));
---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    let count = weakMap.get(endpoint);
+    count++;
+    weakMap.set(endpoint, count);
+
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+
+  return weakMap.get(endpoint);
+}
